63a5cce981f21788c050bf4208ae28b24f38692d
Vivek Kumar Singh <vivek.reck@gmail.com>
2021-03-24 23:32:48 +0530

    forcollege

--- practics2.js
+++
@@ -1,9 +0,0 @@
-// $('#selection').on('shown.bs.modal', function () {
-//     $('#selection').trigger('focus');
-// })
-
-// $('#selection').trigger('focus');
-
-// $('#selection').modal('show');
-
-$('#selection').modal('toggle');
